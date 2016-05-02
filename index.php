17107d5ee2e5f3bb4bd286868452122af033dd94
mszacillo <mas744@cornell.edu>
2016-05-01 20:03:40 -0400

    First edits

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="Style/style.css">
+		<link rel="stylesheet" type = "text/css" href="Style/stylesheet.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="page1" class="page">
+				<img src="Images/Logo.png" alt="LOGO">
+				<p class = "logo">Learn More</p>
+			</div>
+			<div class="page2" class="page">
+				<p class="title">About</p>
+				<p class="info">Dash is a fast-paced flight action game where you must maneuver around enemies to stay airborne. While traditional platformers use attacks as secondary actions, dashing is used to both navigate through the level and defeat opponents.</p>
+			</div>
+			<div class="page3" class="page">
+				<p class="title">Characters</p>
+				<div class="player">
+					<span>
+						<img src="Images/player.jpg" alt="player">
+					</span>
+					<span>
+						<p class="bio">Your task is simple yet challenging: complete the martial trials set by the gods themselves by dashing through your enemies. Master the art of flight as you explore distant lands on your journey to restore prestige to your fallen master's school.</p>
+					</span>
+				</div>
+				<p class="subtitle">Your Opponents</p>
+				<div class="opponents">
+					<span>
+						<img src="Images/wanderer.jpg" alt="Dash wanderer">
+						<br>
+						"ROAMS"
+						<br>
+						"the sky"
+					</span>
+					<span>
+						<img src="Images/sentinel.jpg" alt="Dash sentinel">
+						<br>
+						"HUNTS"
+						<br>
+						"with sharp sense of smell"
+					</span>
+					<span>
+						<img src="Images/aegis.jpg" alt="Dash aegis">
+						<br>
+						"DEFENDS"
+						<br>
+						"with impenetrable shield"
+					</span>
+					<span>
+						<img src="Images/ballista.jpg" alt="Dash ballista">
+						<br>
+						"SHOOTS"
+						<br>
+						"deadly fireballs"
+					</span>
+				</div>
+			</div>
+			<div class="page4" class="page">
+				<p class="title">Instructions</p>
+			</div>
+			<div class="page5" class="page">
+				<p class="title">Gallery</p>
+			</div>
+			<div class="page6" class="page">
+				<p class="title">Team</p>
+			</div>
+			<div class="page7" class="page">
+				<p class="title">Footer</p>
+			</div>
+		</div>
+	</body>	
+</html
