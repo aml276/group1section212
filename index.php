c0340d261be7bc00834a52e975c2ec1b65a7b734
Eric Li <lieric96@gmail.com>
2016-05-02 14:47:03 -0400

    changed html to validate

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<title>Dash - A fast paced flight action game coming soon to Android, iOS, and PC</title>
 		<link rel="stylesheet" type="text/css" href="Style/style.css">
-		<link rel="stylesheet" type = "text/css" href="Style/stylesheet.css">
 	</head>
 	<body>
 
@@ -16,66 +15,61 @@
         </div> <!--end navbar div-->
 
 		<div class="contain">
-			<div id="home" class="page1" class="page">
+			<div id="home" class="page page1">
 				<img src="Images/Logo.png" alt="LOGO">
 				<p class = "logo">Learn More</p>
 			</div>
-			<div id ="about" class="page2" class="page">
+			<div id ="about" class="page page2">
 				<p class="title">About</p>
 				<p class="info">Dash is a fast-paced flight action game where you must maneuver around enemies to stay airborne. While traditional platformers use attacks as secondary actions, dashing is used to both navigate through the level and defeat opponents.</p>
 			</div>
-			<div id="characters" class="page3" class="page">
+			<div id="characters" class="page page3" >
 				<p class="title">Characters</p>
 				<div class="player">
-					<span>
 						<img src="Images/player.jpg" alt="player">
-					</span>
-					<span>
 						<p class="bio">Your task is simple yet challenging: complete the martial trials set by the gods themselves by dashing through your enemies. Master the art of flight as you explore distant lands on your journey to restore prestige to your fallen master's school.</p>
-					</span>
+					
 				</div>
+				<br>
 				<p class="subtitle">Your Opponents</p>
 				<div class="opponents">
 					<span>
 						<img src="Images/wanderer.jpg" alt="Dash wanderer">
-						<br>
 						"ROAMS"
 						<br>
 						"the sky"
 					</span>
 					<span>
 						<img src="Images/sentinel.jpg" alt="Dash sentinel">
-						<br>
 						"HUNTS"
 						<br>
 						"with sharp sense of smell"
 					</span>
 					<span>
 						<img src="Images/aegis.jpg" alt="Dash aegis">
-						<br>
 						"DEFENDS"
 						<br>
 						"with impenetrable shield"
 					</span>
 					<span>
 						<img src="Images/ballista.jpg" alt="Dash ballista">
-						<br>
 						"SHOOTS"
 						<br>
 						"deadly fireballs"
 					</span>
 				</div>
+				
 			</div>
-			<div id="instructions" class="page4" class="page">
+			<div id="instructions" class="page page4" >
 				<p class="title">Instructions</p>
 			</div>
-			<div id="gallery" class="page5" class="page">
+			<div id="gallery" class="page page5" >
 				<p class="title">Gallery</p>
 			</div>
-			<div id="team" class="page6" class="page">
+			<div id="team" class="page page6" >
 				<p class="title">Team</p>
 			</div>
-			<div id="contact" class="page7" class="page">
+			<div id="contact" class="page page7" >
 				<p class = "formtitle">Contact Us</p>         
 
             	<form id="testForm" class = "pure-form pure-form-stacked" method="post" action="#" name = "submit">
@@ -88,4 +82,4 @@
 			</div>
 		</div>
 	</body>	
-</html
+</html>
