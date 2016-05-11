2b731db36bd4422e89029b1d8c3ca9d0e635105f
Austin Lin <austinlin@Austins-MacBook-Pro.local>
2016-05-10 20:47:18 -0400

    implemented contact

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,8 +1,9 @@
-<!--
+<?php
 if isset($_POST['submit']){
-	name = $_POST['name'];
-	email = $_POST['email'];
-	message = $_POST['message'];
-	email name + email + message to client specific email address. 
+	include('config.php');
+	$name = htmlentities(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING));
+	$email = htmlentities(filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING));
+	$message = htmlentities(filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING));
+	mail(CONTACT_EMAIL, "$name: $email", "$message");
 }
--->
+?>
